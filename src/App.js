0a647b2d1e06c06a2fb9d7a27b06b7c69b0a73a8
Sheuli Akter Shathy <sheuliakter85@gmail.com>
2022-11-24 01:10:15 +0600

    basic setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import { RouterProvider } from 'react-router-dom';
+import './App.css';
+import router from './Routes/Routes/Routes';
+
+function App() {
+  return (
+    <div className="container mx-auto">
+         <RouterProvider router={router}></RouterProvider>
+         {/* <Toaster></Toaster> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Shared/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+           jhjj 
+        </div>
+    );
+};
+
+export default Footer;
